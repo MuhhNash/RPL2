70341e610186afd7eb5e5586d7adefa71134c5b3
Muhammad Nazaruddin <muhh.nash@gmail.com>
2022-11-23 20:57:33 +0700

    LA Pert5

--- src/com/toko/bunga/service/TokoBungaService.java
+++ src/com/toko/bunga/service/TokoBungaService.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.toko.bunga.dao;
+
+package com.toko.bunga.service;
 
 import com.toko.bunga.model.TokoBunga;
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
  *
  * @author Anas
  */
-public interface TokoBungaDao {
+public interface TokoBungaService {
     public void save(TokoBunga tokobunga);
     public void update(TokoBunga tokobunga);
     public void delete(TokoBunga tokobunga);
